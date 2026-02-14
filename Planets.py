0374ed22618ee39d2239cb4ff5006c5701c1467d
SantiagoELC-Git <sflopez100@gmail.com>
2026-02-14 13:30:55 -0500

    modified:   Planets.py 	modified:   __pycache__/enemy.cpython-312.pyc 	modified:   app.py

---
+++ Planets.py
@@ -0,0 +1,8 @@
+class Planets( )
+    def __init__(self, mass: float, radius: float, Image: str)
+        self.mass = mass
+        self.radius = radius
+        self.image = Image
+    def retrieve_image:
+        return self.image
+

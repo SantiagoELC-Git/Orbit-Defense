ab45ed75d9e06ce6e7259118ffca5dc3de34d85c
Elizabeth Santosa <elizabethsantosa06@gmail.com>
2026-02-14 16:19:39 -0500

    Add enemy2.py

---
+++ game2.py
@@ -0,0 +1,127 @@
+import pygame as pg
+from enemy2 import Enemy, Planet
+from world import World
+from turret import Turret
+from buttons import Button
+import constants as c
+from pygame import Vector2
+import math
+
+# Initialize Pygame
+pg.init()
+
+# Control FPS
+clock = pg.time.Clock()
+
+# Create a game window
+screen = pg.display.set_mode((c.SCREEN_WIDTH + c.SIDE_PANEL, c.SCREEN_HEIGHT))
+pg.display.set_caption("Orbit Defense")
+
+# Load images
+# map
+map_img = pg.image.load('Pixel-Art/Background/space.png').convert_alpha()
+# individual turrent image under cursor
+cursor_turret1 = pg.image.load('Pixel-Art/Turrets/turret_placeholder.png').convert_alpha()
+# enemies
+meteor1_img = pg.image.load('Pixel-Art/Enemies/meteor1.png').convert_alpha()
+# buttons
+buy_heisenberg_img = pg.image.load('Pixel-Art/Buttons/HEISENBERG.png').convert_alpha()
+cancel_button_img = pg.image.load('Pixel-Art/Buttons/Cancel_Button.png').convert_alpha()
+
+def create_turret(mouse_pos):
+    mouse_tile_x = mouse_pos[0] // c.TILE_SIZE
+    mouse_tile_y = mouse_pos[1] // c.TILE_SIZE
+    # Calculate the sequential tile number
+    #mouse_tile_num = 
+    turret = Turret(cursor_turret1, mouse_pos)
+    turret_group.add(turret)
+
+# create world
+world = World(map_img)
+
+# create groups
+enemy_group = pg.sprite.Group()
+turret_group = pg.sprite.Group()
+
+# create buttons
+heisenberg_button = Button(c.SCREEN_WIDTH + 100, 50, buy_heisenberg_img) 
+cancel_button = Button(c.SCREEN_WIDTH + 50, 180, cancel_button_img) 
+
+
+planet = Planet(c.SCREEN_WIDTH // 2, c.SCREEN_HEIGHT // 2, mass=1000)
+# planet1 = Planet(c.SCREEN_WIDTH - 300, c.SCREEN_HEIGHT // 2, mass=1000)
+
+celestial_objects = [planet]
+
+waypoints = [
+    (100, 200),
+    (300, 100),
+    (400, 400),
+    (67, 67)
+]
+
+meteor1 = Enemy(waypoints, meteor1_img, 1.0)
+
+
+# Give perpendicular velocity for circular orbit
+# r_pixels = 200
+# r_meters = r_pixels * Enemy.SCALE
+# v_circular = math.sqrt(Enemy.G * planet.mass / r_meters)
+
+# Convert m/s → pixels per frame
+# meteor1.vel = Vector2(0, -v_circular / Enemy.SCALE)
+
+enemy_group.add(meteor1)
+
+# Makes game run
+run = True
+while run:
+
+    clock.tick(c.FPS) # Set the frame rate to 60 
+
+    #####
+    # Updating
+    #####
+
+    # update groups
+    enemy_group.update(celestial_objects)
+
+    #####
+    # Drawing
+    #####
+
+    screen.fill('gray100')
+
+    # draw level
+    world.draw(screen)
+
+    # draw enemy path
+    pg.draw.lines(screen, 'black', False, waypoints)
+
+    # draw groups
+    enemy_group.draw(screen)
+    turret_group.draw(screen)
+
+    # draw buttons
+    # button for placing turrets
+    if heisenberg_button.draw(screen):
+        print ("heisenberg button clicked")
+    if cancel_button.draw(screen):
+        print("cancel button clicked")
+
+    #event handler
+    for event in pg.event.get():
+        #quit game
+        if event.type == pg.QUIT:
+            run = False
+        # mouse click
+        if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+            mouse_pos = pg.mouse.get_pos()
+            # check if mouse is within bounds of the screen
+            if mouse_pos[0] < c.SCREEN_WIDTH and mouse_pos[1] < c.SCREEN_HEIGHT:
+                create_turret(mouse_pos)
+
+    # Update the display
+    pg.display.flip()
+# Quit Pygame
+pg.quit()
